31210276eea4440837948dd3d3b26a091000a9ff
Guilherme Borges <guilhermepardi@gmail.com>
2023-03-01 10:28:35 -0300

    Apagando alguns arquivos que não serão úteis.
    Arquivos que foram originados no create-react-app.

--- app-organo/src/App.js
+++ app-organo/src/App.js
@@ -1,27 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 import Banner from './Compontentes/Banner/Banner';
 
 function App() {
   return (
 
     <div className="App">
-
       <Banner/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Edicted.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
