6c6d009bd9e82adc723b3c0856b4b5e57c0c532a
Guilherme Borges <guilhermepardi@gmail.com>
2023-03-01 09:51:46 -0300

    Subindo a primeira parte do projeto.
    Banner da parte de cima do site.

--- app-organo/src/App.js
+++ app-organo/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './Compontentes/Banner/Banner';
 
 function App() {
   return (
+
     <div className="App">
+
+      <Banner/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ app-organo/src/Compontentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import "./Banner.css"
+
+function Banner () {
+    return (
+        <header className="banner">
+            <img src="../imagens/banner.png" alt="Imagem do Banner" className="banner-img"/>
+        </header>
+    )
+}
+
+export default Banner
