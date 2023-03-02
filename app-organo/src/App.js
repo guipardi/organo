8e923fb0d9472e41443b2d8d8a0339901d7b44bd
Guilherme Borges <guilhermepardi@gmail.com>
2023-03-01 21:27:31 -0300

    Subindo mais uma parte dos arquivos do projeto.

--- app-organo/src/App.js
+++ app-organo/src/App.js
@@ -1,10 +1,14 @@
-import Banner from './Compontentes/Banner/Banner';
+import Banner from './Compontentes/Banner';
+import CampoTexto from './Compontentes/CampoTexto';
 
 function App() {
   return (
 
     <div className="App">
       <Banner/>
+      <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
+      <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
+      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
     </div>
   );
 }
