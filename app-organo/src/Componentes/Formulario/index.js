6fdda300d4248e342fad83062a945afef8699291
Guilherme Borges <guilhermepardi@gmail.com>
2023-03-07 22:47:18 -0300

    Atualizando index.js

--- app-organo/src/Componentes/Formulario/index.js
+++ app-organo/src/Componentes/Formulario/index.js
@@ -31,7 +31,7 @@ const Formulario = (props) => {
         imagem : imagem,
         time : time
     })
-
+ 
     return(
         <section className='formulario'>
             <form onSubmit={aoSalvar}>
